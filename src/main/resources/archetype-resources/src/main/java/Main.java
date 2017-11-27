bbcd35581935e4c46f5e1c36c2d33626d06e638c
lastsecondsave <lastsecondsave@outlook.com>
2017-11-27 23:43:31 +0300

    First version of files

---
+++ src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,14 @@
+package ${package};
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.function.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+}
